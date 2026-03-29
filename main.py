6f48164481dc9f8f222376f00f875fa9d82d954c
ayan420a <work.ayan27@gmail.com>
2026-03-29 14:02:09 +0530

    backend ready for railway

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Backend running"}
+    
 import os
 from datetime import datetime
 from typing import List
